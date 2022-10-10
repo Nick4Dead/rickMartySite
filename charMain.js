b35e3fbfa1b9ed5e2ca21608e5ea16fe3a7f5e9c
Nick4Dead <nickolas19994@gmail.com>
2022-10-10 15:36:14 +0500

    Исправил комментарии

--- charMain.js
+++ charMain.js
@@ -1,18 +1,23 @@
-async function getImgChar() {
+async function getImagesFromApi() {
   let char = await fetch("https://rickandmortyapi.com/api/character");
   let data = await char.json();
+  const sliced = data.results.slice(0, 5);
+  const arrWithImg = [];
+  for (item of sliced) {
+    arrWithImg.push(item.image);
+  }
+  return arrWithImg;
+}
+
+async function addToHtml() {
+    let arrFromApi = await getImagesFromApi()
   const containerDiv = document.createElement("div");
   containerDiv.classList.add("charImg");
 
   let container = document.getElementById("cont");
   container.appendChild(containerDiv);
 
-  const sliced = data.results.slice(0, 5);
-  const arrWithImg = [];
-  for (let i = 0; i < sliced.length; i++) {
-    arrWithImg.push(sliced[i].image);
-  }
-  for (item of arrWithImg) {
+  for (item of arrFromApi) {
     let link = document.createElement("a");
     link.href = "ссылка на персонажа";
     let image = document.createElement("img");
@@ -21,4 +26,4 @@ async function getImgChar() {
     containerDiv.appendChild(link);
   }
 }
-getImgChar();
+addToHtml()
