fbffba5d5f61455d14044170102fb556f15755d5
Nick4Dead <nickolas19994@gmail.com>
2022-10-11 16:35:15 +0500

    Отредактировал текст

--- episodes.js
+++ episodes.js
@@ -1,39 +1,33 @@
 async function getEpisodeInfo() {
   let episode = await fetch("http://193.124.118.129:7899/seasons/0");
   let data = await episode.json();
-  return data
+  return data;
 }
 
 async function pushToHtml() {
   const episodes = await getEpisodeInfo();
-  
+
   const containerId = document.getElementById("containerForImages");
-  
-  for(let episode of episodes) {
+
+  for (let episode of episodes) {
     const container = document.createElement("div");
     container.classList.add("episodeImg");
 
-
     const img = document.createElement("img");
-    img.classList.add("picture")
+    img.classList.add("picture");
     img.src = episode.image;
     container.appendChild(img);
 
+    const descrip = document.createElement("p");
+    descrip.innerText = episode.description;
+    descrip.classList.add("description");
+    container.appendChild(descrip);
+    const h = document.createElement("h3");
+    h.innerText = episode.name;
 
-    const descrip = document.createElement('p')
-    descrip.innerText = episode.description
-    descrip.classList.add('description')
-    container.appendChild(descrip)
-    const h = document.createElement("h3")
-    h.innerText = "N Эпизод"
-
-    // h.setAttribute("id","h")
-    // document.querySelector("h").innerText = 'N Эпизод'
-    descrip.prepend(h)
-
+    descrip.prepend(h);
 
     containerId.appendChild(container);
-
   }
 }
 pushToHtml();
