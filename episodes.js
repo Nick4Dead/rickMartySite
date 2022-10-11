48d92d8c8244474a3466466251abbd06b5ed7be7
Nick4Dead <nickolas19994@gmail.com>
2022-10-11 13:49:04 +0500

    Добавил анимацию к эпизодам

--- episodes.js
+++ episodes.js
@@ -1,26 +1,47 @@
 async function getEpisodeInfo() {
   let episode = await fetch("http://193.124.118.129:7899/seasons/0");
   let data = await episode.json();
-  const arrWithImg = [];
-  for (item of data) {
-    arrWithImg.push(item.image);
-  }
-  return arrWithImg;
+  // const arrWithImg = [];
+  // for (item of data) {
+  //   arrWithImg.push(item.image);
+  //   arrWithImg.push(item.description)
+  //   console.log(arrWithImg);
+
+  // }
+  // return arrWithImg;
+  return data
 }
 
 async function pushToHtml() {
-  const arrOfImages = await getEpisodeInfo();
-  const container = document.createElement("div");
-  container.classList.add("episodeImg");
-
+  const episodes = await getEpisodeInfo();
+  
   const containerId = document.getElementById("containerForImages");
-  containerId.appendChild(container);
+  
+  for(let episode of episodes) {
+    const container = document.createElement("div");
+    container.classList.add("episodeImg");
 
-  for (item of arrOfImages) {
+
     const img = document.createElement("img");
     img.classList.add("picture")
-    img.src = item;
+    img.src = episode.image;
     container.appendChild(img);
+
+
+    const descrip = document.createElement('p')
+    descrip.innerText = episode.description
+    descrip.classList.add('description')
+    container.appendChild(descrip)
+
+    
+    containerId.appendChild(container);
+
   }
+
+  // for (let i=0;i<arrOfImages.length;i++) {
+  //   if (arrOfImages[i]%2==0){
+  //   }else{
+  //   }
+  // }
 }
 pushToHtml();
