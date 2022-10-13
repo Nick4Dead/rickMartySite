95ffba14594f407b04d091b680f671117e2a0e04
Nick4Dead <nickolas19994@gmail.com>
2022-10-13 13:32:44 +0500

    test

--- src/app.js
+++ src/app.js
@@ -1,7 +1,24 @@
 import React from "react";
 
 export function App() {
-    return <div>
-        TEST
-    </div>
+  return (
+    <>
+      <a href="/index.html">
+        <img class="logo" src="https://i.yapx.ru/UN37v.png" alt="logo" />
+      </a>
+      <ul class="menu">
+        <li>
+          <a href="/charPage.html">Персонажи</a>
+        </li>
+        <li>
+          <a href="/episodes.html">Эпизоды</a>
+        </li>
+      </ul>
+      <img
+        class="pageImg"
+        src="https://i.yapx.ru/UN37y.png"
+        alt="mainPageImg"
+      />
+    </>
+  );
 }
